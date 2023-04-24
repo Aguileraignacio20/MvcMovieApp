8b0181c655b68aefc7f42a9bf27d0cc596ffa9ef
Aguileraignacio20 <130873718+Aguileraignacio20@users.noreply.github.com>
2023-04-23 21:48:46 -0300

    CLASE 3
    Actividad de asistencia CLASE 3 -  tutorial de MVC - Views

---
+++ HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Text.Encodings.Web;
+
+//namespace MvcMovie.Controllers;
+
+//public class HelloWorldController : Controller
+//{
+//    // 
+//    // GET: /HelloWorld/
+//    public IActionResult Index()
+//    {
+//        return View();
+//    }
+
+//public string Welcome(string name, int ID = 1)
+//    {
+//        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+//    }
+// 
+//
+//}
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
